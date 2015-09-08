18a11b91881a95a5f67de8f5130d1c41551857a9
Archie Fielding <archiefielding@gmail.com>
2015-09-08 14:05:45 +0100

    Boat 2 status update

--- lib/ship_2.rb
+++ lib/ship_2.rb
@@ -3,17 +3,40 @@ class Ship_2
     @makeup = [0, 0]
     @floating = true
   end
+
   def floating?
     @floating
   end
-  def hit
-    @makeup = [1,0]
+
+  def sunk?
+    not floating?
+  end
+
+  def hit(number)
+    @makeup[number] = 1
+    status_update
+    @makeup[number]
+  end
+
+  def status_update
+    if status == "Sunk"
+      @floating = false
+    else
+      @floating = true
+    end
+  end
+
+  def check
     @makeup.inject { |sum, x| sum + x }
   end
-  def check
-    true
-  end
+
   def status
-    "All fine"
+    if check == 0
+      "All fine"
+    elsif check < @makeup.length
+      "Damaged"
+    else
+      "Sunk"
+    end
   end
 end
--- spec/ship_2_spec.rb
+++ spec/ship_2_spec.rb
@@ -2,15 +2,16 @@ require 'ship_2'
 describe Ship_2 do
 
     it "should be floating when created" do
-    	expect(subject).to be_floating
+      expect(subject).to be_floating
     end
+    
     describe "#hit" do
       it 'can be hit' do
         expect(subject).to respond_to :hit
       end
 
       it "returns damage" do
-        expect(subject.hit).to eq(1)
+        expect(subject.hit(0)).to eq(1)
       end
     end
 
@@ -23,32 +24,34 @@ describe Ship_2 do
     end
 
     it "can check its makeup" do
-      subject.hit
-      expect(subject.check).to eq(true)
-    end
-    xit "can check its makeup" do
-      expect(subject.check).to eq(false)
+      subject.hit(0)
+      expect(subject.check).to eq(1)
     end
 
-    # it 'Ship can respond with status "Sunk"' do
-    #   subject.hit
-    # 	expect(subject.status).to eq("Sunk")
-    # end
-    #
+    it "can check its all fine" do
+      expect(subject.check).to eq(0)
+    end
 
-    # # it "can't be floating if sunk" do
-    # # 	subject.sunk?
-    # # 	expect(subject).
-    # # end
-    #
+    it 'Ship can respond with status "Sunk"' do
+      subject.hit(0)
+      subject.hit(1)
+      expect(subject.status).to eq("Sunk")
+    end
 
+    it "can't be floating if sunk" do
+    	subject.sunk?
+    	expect(subject.floating?).to eq(true)
+    end
 
-    # it 'can report being hit' do
-    # 	expect(subject.hit).to eq("Hit")
-    # end
+    it "Ship sinks if each part is hit" do
+      subject.hit(0)
+      subject.hit(1)
+      expect(subject.sunk?).to eq(true)
+    end
 
-    # it "Ship sinks if each part is hit" do
-    #   subject.hit
-    #   expect(subject.sunk?).to eq(true)
-    # end
+    it "checks status after every hit" do
+      subject.hit(0)
+      expect(subject.status).to eq("Damaged")
+    end
+
 end
